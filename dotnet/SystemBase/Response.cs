86edc70a6af874781b176fb6fef0040e5823c97d
John Josua Paderon <johnjosua.paderon@outlook.com>
2023-12-21 23:34:28 +0800

    Contract: Verify Permission Failed

--- dotnet/SystemBase/Response.cs
+++ dotnet/SystemBase/Response.cs
@@ -7,3 +7,8 @@ public interface ICQRSErrorResponse : ICQRSResponse {}
 public sealed record Success : ICQRSResponse {}
 
 public sealed record Error : ICQRSErrorResponse {}
+
+public sealed record VerifyPermissionFailed : ICQRSErrorResponse
+{
+    public int PermissionId { get; init; }
+}
