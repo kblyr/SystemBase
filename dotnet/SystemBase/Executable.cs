4def85a781ffd607823c388653cdc2b2442a6d7f
John Josua Paderon <johnjosua.paderon@outlook.com>
2024-04-24 16:07:44 +0800

    implementing interfaces

--- dotnet/SystemBase/Executable.cs
+++ dotnet/SystemBase/Executable.cs
@@ -7,9 +7,21 @@ public interface IExecutable<TResult> : IDisposable
 
 public interface IExecutable : IExecutable<Unit> {}
 
-public readonly struct Unit
+public readonly struct Unit : IEquatable<Unit>, IComparable<Unit>
 {
     static readonly Unit _value = new();
 
     public static Unit Value => _value;
+
+    public int CompareTo(Unit other) => 0;
+
+    public bool Equals(Unit other) => true;
+
+    public override bool Equals(object? obj) => true;
+
+    public override int GetHashCode() => 0;
+
+    public static bool operator ==(Unit _, Unit __) => true;
+
+    public static bool operator !=(Unit _, Unit __) => false;
 }
